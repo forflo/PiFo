604f2bb81246bbb1d397fb2be5095c6759675dc8
Florian Mayer <mayer.florian@web.de>
2016-01-28 18:47:03 +0100

    Added missing files

---
+++ latex_filegenerator.c
@@ -0,0 +1,15 @@
+
+	      //  fprintf(transcript_file, HEADER HEADER_FCOLOR "{%s}" 
+          //          HEADER_BCOLOR "{%s}" HEADER_DOC BEG_MATH "%s" END_MATH
+          //          FOOTER, fgcolor, bgcolor, latex_expression);
+static 
+
+static gboolean generate_latex_lstlisting(GString *listing, 
+
+        GString *language, const char *filename);
+
+static gboolean generate_latex_formula(GString *formula, 
+        const char *filename);
+
+
+
